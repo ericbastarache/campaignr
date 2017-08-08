80dfeff31c0a9348839b91842c05cc45380a267a
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-08 00:04:35 -0400

    Updated server entry point

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express');
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./services/passport");
+require("./models/User");
 
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
